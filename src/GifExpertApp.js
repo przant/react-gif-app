df9c7cd5a84a29a84679f565e877c77f215c758b
Antonio Pérez <apema2079@gmail.com>
2021-11-17 13:52:24 -0600

    Add hook to GifExpertApp component

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+    const handleAdd = () => {
+        const anime = 'The Seven Deadly Sins';
+        setCategories([
+            ...categories,
+            anime,
+        ]);
+    }
+
     return (
         <div>
             <h2>GifExpertApp</h2>
             <hr></hr>
+
+            <button onClick={ handleAdd }>Add Anime</button>
+
+            <ol>
+                {
+                    categories.map(
+                        (category) => {
+                            return <li key={ category }>{ category }</li>
+                        }
+                    )
+                }
+            </ol>
         </div>
     )
 }
